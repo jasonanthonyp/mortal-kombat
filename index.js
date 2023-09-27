b2c82870f330fef482768dfe6ee5cda8cd8a6896
Jason Phillips <jasonanthonyp@gmail.com>
2023-09-27 11:29:24 -0400

    likes update

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
+
+let selectedCharacter
+
+
+function getLikes(id) {
+    fetch("http://localhost:3000/characters/" + id)
+    .then(response => response.json())
+    .then(character => {
+        document.querySelector("#vote-count").textContent = character.votes
+    })
+    
+
+}
+
+
+
+
+
+
 fetch("http://localhost:3000/characters")
 .then(response => response.json())
 .then(characters => {
     //console.log(characters)
+    selectedCharacter = characters[0]
     characters.forEach(character => {
         const kombatImage = document.createElement("img")
         kombatImage.src = character.image
       
 
         kombatImage.addEventListener("click", () => {
+            selectedCharacter = character
             document.querySelector(".detail-image").src = character.image
             document.querySelector("#name").textContent = character.name
             document.querySelector("#description").textContent = character.description
-            document.querySelector("#vote-count").textContent = character.votes
+            getLikes(selectedCharacter.id)
+
         })
           document.querySelector(".character-list").append(kombatImage)
     })
@@ -19,8 +41,41 @@ fetch("http://localhost:3000/characters")
     document.querySelector(".detail-image").src = characters[0].image
     document.querySelector("#name").textContent = characters[0].name
     document.querySelector("#description").textContent = characters[0].description
+    document.querySelector("#vote-count").textContent = characters[0].votes
+
+})
 
 
+    
+    
+
+document.querySelector("#submit-likes").addEventListener("submit", event => {
+    event.preventDefault();
+
+    //console.log("hello")
+   
+    const likeButton = document.querySelector("#skull")
+    let newLikeButton = document.querySelector("#vote-count")
+
+
+    //count++
+
+    //newLikeButton.innerText = count
+
+    let updateVotes = selectedCharacter.votes
+
+    console.log(selectedCharacter)
+    fetch("http://localhost:3000/characters/" + selectedCharacter.id), {
+        
+        method: "PATCH",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify ({
+            votes: ++updateVotes
+        })
+    }
+    
 })
 
 
