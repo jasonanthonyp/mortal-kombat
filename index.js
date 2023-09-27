26349627c2753b7d3a1601e489ac6fa8f8c34ffd
Jason Phillips <jasonanthonyp@gmail.com>
2023-09-27 13:49:37 -0400

    almost done

--- index.js
+++ index.js
@@ -1,83 +1,51 @@
+const charList = document.querySelector(".character-list");
+let selectedId;
 
-let selectedCharacter
-
-
-function getLikes(id) {
-    fetch("http://localhost:3000/characters/" + id)
-    .then(response => response.json())
-    .then(character => {
-        document.querySelector("#vote-count").textContent = character.votes
-    })
-    
-
+function showDetails(char) {
+    selectedId = char.id;
+    fetch(`http://localhost:3000/characters/${selectedId}`)
+        .then(res => res.json())
+        .then(character => {
+            document.querySelector(".detail-image").src = character.image;
+            document.querySelector("#name").textContent = character.name;
+            document.querySelector("#description").textContent = character.description;
+            document.querySelector("#vote-count").textContent = character.votes;
+        })
 }
 
-
-
-
-
+function listChar(char) {
+    const kombatImage = document.createElement("img");
+    kombatImage.src = char.image;
+    kombatImage.addEventListener("click", (e) => {
+        e.preventDefault();
+        selectedId = char.id;
+        showDetails(char);
+    })
+    charList.append(kombatImage);
+};
 
 fetch("http://localhost:3000/characters")
-.then(response => response.json())
-.then(characters => {
-    //console.log(characters)
-    selectedCharacter = characters[0]
-    characters.forEach(character => {
-        const kombatImage = document.createElement("img")
-        kombatImage.src = character.image
-      
+    .then(res => res.json())
+    .then(characters => {
+        showDetails(characters[0]);
+        characters.forEach(character => {
+            listChar(character);
+        });
+    });
 
-        kombatImage.addEventListener("click", () => {
-            selectedCharacter = character
-            document.querySelector(".detail-image").src = character.image
-            document.querySelector("#name").textContent = character.name
-            document.querySelector("#description").textContent = character.description
-            getLikes(selectedCharacter.id)
-
-        })
-          document.querySelector(".character-list").append(kombatImage)
-    })
-
-    document.querySelector(".detail-image").src = characters[0].image
-    document.querySelector("#name").textContent = characters[0].name
-    document.querySelector("#description").textContent = characters[0].description
-    document.querySelector("#vote-count").textContent = characters[0].votes
-
-})
-
-
-    
-    
-
-document.querySelector("#submit-likes").addEventListener("submit", event => {
-    event.preventDefault();
-
-    //console.log("hello")
-   
-    const likeButton = document.querySelector("#skull")
-    let newLikeButton = document.querySelector("#vote-count")
-
-
-    //count++
-
-    //newLikeButton.innerText = count
-
-    let updateVotes = selectedCharacter.votes
-
-    console.log(selectedCharacter)
-    fetch("http://localhost:3000/characters/" + selectedCharacter.id), {
-        
+document.getElementById("submit-likes").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let voteCount = document.getElementById("vote-count");
+    let newVotes = parseInt(voteCount.textContent);
+    newVotes++;
+    fetch(`http://localhost:3000/characters/${selectedId}`, {
         method: "PATCH",
         headers: {
             "Content-type": "application/json"
         },
-        body: JSON.stringify ({
-            votes: ++updateVotes
+        body: JSON.stringify({
+            votes: newVotes
         })
-    }
-    
-})
-
-
-
-
+    });
+    voteCount.textContent = newVotes;
+});
